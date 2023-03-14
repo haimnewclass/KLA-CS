0f57522085bf1ee81d6c474e91674784c6224e7a
haim hod gilboa <haim@newclass.co.il>
2023-03-14 11:55:55 +0200

    Loops

--- KLA-Console/Program.cs
+++ KLA-Console/Program.cs
@@ -37,6 +37,52 @@ namespace KLA_Console
 
             Console.WriteLine(res);
 
+            int num1 = 10;
+            int num2 = 20;
+            bool cond = true;
+
+            cond = num1 > num2;
+
+            if(num1>num2)
+            {
+                Console.WriteLine("Num1 is Big");
+            }
+            else
+            {
+                Console.WriteLine("Num1 is NOT Big");
+            }
+
+            string userInput = "";
+            userInput = Console.ReadLine();
+            while (userInput!="EXIT")
+            {
+                Console.WriteLine(userInput);
+                Console.WriteLine("Please Insert new Value");
+                userInput = Console.ReadLine();
+            }
+
+            int y = 9;
+            y = y + 1;
+            y++;
+
+            y = y - 1;
+            y--;
+
+            y = y + 90;
+            y += 90;
+
+            y = y - 80;
+            y -= 80;
+
+
+
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
             Console.ReadLine();
         }
     }
