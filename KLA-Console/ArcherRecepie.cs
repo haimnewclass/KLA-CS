eb670299b36b1e063b9b871d9e4c811c40b09f86
haimnewclass <haim@newclass.co.il>
2023-03-19 14:51:02 +0200

    Sample

---
+++ KLA-Console/ArcherRecepie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLA_Console
+{
+    internal class ArcherRecepie
+    {
+        public string Name;
+        public bool Ported;
+        public DateTime Date;
+
+        public void Run()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
