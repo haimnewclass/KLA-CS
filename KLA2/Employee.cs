a351925e5bfdcd9153759023dfd9de313d204be3
haim hod gilboa <haim@newclass.co.il>
2023-03-21 15:26:03 +0200

    1

---
+++ KLA2/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace KLA2.A
+{
+    public class Employee
+    {
+        public int Taz { get; set; }
+        public string Name { get;set; }
+        public int EmpId { get; set; }
+
+    }
+
+    public class Manager
+    {
+        public Employee[] list = new Employee[1000];
+        public Employee CreateEmployee(int Taz, string Name,int EmpId)
+        {
+            Employee ret = new Employee();
+            ret.Taz = Taz;
+            ret.Name = Name;
+            ret.EmpId = EmpId;
+
+            return ret;
+        }
+
+        public void AddEmp(Employee emp)
+        {
+            list[emp.EmpId] = emp;
+        }
+    }
+
+}
--- KLA2/Manager.cs
+++ KLA2/Manager.cs
@@ -3,31 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KLA2.A;
 
 namespace KLA2
 {
 
     internal class Manager
     {
-
+        public Employee employee;
         public BeerCup[] arr = new BeerCup[10];
+        
         public void Run()
         {
             for (int i = 0; i < 10; i++)
             {
                 BeerCup beerCup = new BeerCup();
                 beerCup.Capacity = 10;
-                AddCup(beerCup,i);
+                AddCup(beerCup, i);
             }
         }
 
         public int currCup = 0;
-        public void AddCup(BeerCup beerCup,int key)
+        public void AddCup(BeerCup beerCup, int key)
         {
             if (key >= 10) return;
 
             arr[key] = beerCup;
-             
+
         }
         public BeerCup GetCup(int key)
         {
