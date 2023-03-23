d9ef6a5cdd9e0793c43eb83e7cf3b565a01dfc8d
haimnewclass <haim@newclass.co.il>
2023-03-23 16:51:18 +0200

    KLA

---
+++ KLA4-InpuitDataFromUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KLA4_InpuitDataFromUser;
+
+Console.WriteLine("Hello, World!");
+InputData inputData = new InputData();
+inputData.InputDataFromFile();
