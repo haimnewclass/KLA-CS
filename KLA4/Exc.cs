b66430fd23a92953f1ee136c15096f73ac950c1d
haim hod gilboa <haim@newclass.co.il>
2023-03-28 10:33:25 +0300

    Exception

---
+++ KLA4/Exc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLA4
+{
+    internal class Exc
+    {
+        public void Run()
+        {
+            int num = 0;
+
+            bool valid = false;
+
+            while (!valid)
+            { 
+                string str = Console.ReadLine();
+                try
+                {
+                    num = int.Parse(str);
+                    valid = true;
+
+                    //System.IO.File.ReadAllText(@"l:\aaa\ggg.txt");
+                    Test(200);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine("Error Was not valid number " + ex.Message);
+
+                }
+                catch (DirectoryNotFoundException ex)
+                {
+                    Console.WriteLine("Format is not valid "+ex.Message);
+                }
+                catch(NotGreatThan100Exception ex)
+                {
+                    Console.WriteLine("Test Greater than 100");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Was not valid number " + ex.Message);
+                }
+                
+                
+                Console.WriteLine("Finaly");
+                
+            }
+
+            Console.WriteLine(num*num);
+            Console.ReadLine();
+
+
+        }
+
+        public void Test(int num)
+        {
+            if(num > 100)
+            {
+                NotGreatThan100Exception ex = new NotGreatThan100Exception("Not Valid more than 100");
+                throw ex;
+            }
+
+            Console.WriteLine("Vali");
+        }
+    }
+
+    class NotGreatThan100Exception : Exception
+    { 
+        public NotGreatThan100Exception(string msg) : base(msg) { }
+    }
+
+}
---
+++ KLA4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KLA4;
+
+Console.WriteLine("Hello, World!");
+Exc Exc = new Exc();
+try
+{
+    Exc.Run();
+}
+catch(Exception ex)
+{
+
+}
